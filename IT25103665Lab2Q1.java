034fffff026aeef3c0a462bfdfc2f5de7018528f
Sankalpani56 <eesirasankalpanigds@gmail.com>
2026-01-07 09:28:45 +0530

    Added Lab2 Q1 Answer

---
+++ IT25103665Lab2Q1.java
@@ -0,0 +1,13 @@
+public class IT25103665Lab2Q1 {
+    public static void main(String[] args){
+
+       double perimeter = 100.0;
+       double length = (perimeter/2)/1.75;
+       double width = length*(3.0/4.0);
+        
+
+        System.out.println("Length of the fence :" + length);
+        System.out.println("Width of the fence : " + width);
+
+    }
+}
