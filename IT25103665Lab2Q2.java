99c70110c1a88f1310fb9feb61506af9834e5b8b
Sankalpani56 <eesirasankalpanigds@gmail.com>
2026-01-11 22:02:39 +0530

    Added Lab2Q2 Answer

---
+++ IT25103665Lab2Q2.java
@@ -0,0 +1,14 @@
+public class IT25103665Lab2Q2 {
+    public static void main(String[] args) {
+        
+        double length = 10;
+        double perimeter =  4*length;
+        double pi = 3.14;
+
+        
+        
+        double radius = perimeter/(2*pi);
+
+        System.out.println("Radius of the circular fence : " + radius);
+    }
+}
