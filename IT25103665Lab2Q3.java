49382bcd9349cc54f634ca25ed0eeb839793ea2e
Sankalpani56 <eesirasankalpanigds@gmail.com>
2026-01-12 20:27:29 +0530

    Added Lab2Q3 Answer

---
+++ IT25103665Lab2Q3.java
@@ -0,0 +1,11 @@
+public class IT25103665Lab2Q3 {
+    public static void main(String[] args) {
+        
+        double A = 3.0;
+        double B = 4.0;
+
+        double Hypotenuse = Math.sqrt((A*A)+(B*B));
+
+        System.out.println("Length of the hypotenuse :" + Hypotenuse);
+    }
+}
